e1e6811067bfe8c4aa41105301c6c47d90c2bc88
Thanh Nguyen <thankz173@gmail.com>
2022-02-17 15:32:17 +0700

    fix: update member detail

--- app/Controllers/Http/GuildMembersController.ts
+++ app/Controllers/Http/GuildMembersController.ts
@@ -111,7 +111,14 @@ export default class GuildBackendsController {
   public async getMembers({ request, response }: HttpContextContract) {
     const wallet = request.param('wallet')
     if (wallet) {
-      const userRecord = await UserBackend.findBy('address', wallet)
+      const userRecord = await Database.from('user_backends')
+        .join('users', 'users.user_id', '=', 'user_backends.discord_id')
+        .where('user_backends.address', wallet)
+        .select('user_backends.*')
+        .select('users.username')
+        .select('users.discriminator')
+        .first()
+
       if (!userRecord) {
         response.notFound({
           statusCode: 404,
@@ -119,10 +126,20 @@ export default class GuildBackendsController {
         })
         return
       }
+
       response.ok({
         statusCode: 200,
         message: 'successfully',
-        data: userRecord,
+        data: {
+          role: userRecord?.role,
+          address: userRecord?.address,
+          discord_id: userRecord?.discord_id,
+          mecha_own: userRecord?.mecha_own,
+          distance: userRecord?.distance,
+          contribution: userRecord?.contribution,
+          guild_point: userRecord?.guild_point,
+          discordAccount: `${userRecord?.username}#${userRecord?.discriminator}`,
+        },
       })
       return
     }
